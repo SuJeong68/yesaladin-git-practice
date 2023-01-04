11247f6225f3583ea33f27719faf0636c4aef915
SuJeong68 <tidh1640@naver.com>
2023-01-04 12:03:43 +0900

    Initial Commit

---
+++ src/main/java/com/example/yesaladingitpractice/YesaladinGitPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.yesaladingitpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YesaladinGitPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YesaladinGitPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/yesaladingitpractice/YesaladinGitPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yesaladingitpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YesaladinGitPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
